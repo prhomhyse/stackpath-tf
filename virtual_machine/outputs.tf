57e0f5b0cc13705bc6cc5b65a4a50797766ebcc4
akpanpromise@hotmail.com <akpanpromise@hotmail.com>
2020-03-31 00:20:12 +0100

    add full virtual_machine resource and output

--- virtual_machine/outputs.tf
+++ virtual_machine/outputs.tf
@@ -1,7 +1,7 @@
 # output a map of instance name to IP address
 output "my-terraform-workload-instances" {
   value = {
-    for instance in stackpath_compute_workload.my-terraform-nginx-workload.instances:
+    for instance in stackpath_compute_workload.jollofx.instances:
     instance.name => instance.external_ip_address
   }
 }
